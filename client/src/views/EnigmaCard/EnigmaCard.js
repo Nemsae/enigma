50315c496e677e078be38e58ca0db8db02198c97
John Son <jsson77@gmail.com>
2017-06-23 02:49:53 -0700

    add proptype checking for EnigmaCard

--- client/src/views/EnigmaCard/EnigmaCard.js
+++ client/src/views/EnigmaCard/EnigmaCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Card from 'react-toolbox/lib/card/Card';
 import CardActions from 'react-toolbox/lib/card/CardActions';
@@ -11,52 +12,56 @@ const todaysDate = new Date(Date.now());
 const minimumDate = new Date(todaysDate.setDate(todaysDate.getDate() - 1));
 
 //  TODO: Prop validation
-class EnigmaCard extends React.Component {
-  render() {
-    const { sender, message, date, handleChange, handleToggle } = this.props;
-    // console.log('handleChange: ', handleChange);
-    // console.log('handleToggle: ', handleToggle);
-    return (
-      <div>
-        <Card style={{ width: '350px', padding: '15px 15px 0px 15px' }}>
-          Tovias Enigma
-          <Input
-            type='text'
-            label='Name'
-            id='sender'
-            value={sender}
-            icon={<Avatar icon={<span>{sender[0]}</span>} />}
-            onChange={handleChange}
-            required
-          />
-          <Input
-            type='text'
-            label='Message'
-            id='message'
-            value={message}
-            maxLength={120}
-            onChange={handleChange}
-            multiline
-            required
-          />
-          <DatePicker
-            label='Expiration date'
-            minDate={minimumDate}
-            id='date'
-            value={date}
-            autoOk={true}
-            onChange={handleChange}
-            sundayFirstDayOfWeek
-            required
-          />
-          <CardActions>
-            <Button label='ENCRYPT' id='encrypt' onClick={handleToggle} />
-            <Button label='DECRYPT' id='decrypt' onClick={handleToggle} />
-          </CardActions>
-        </Card>
-      </div>
-    );
-  }
-}
+const EnigmaCard = (props) => {
+  const { sender, message, date, handleChange, handleToggle } = props;
+  return (
+    <div>
+      <Card style={{ width: '350px', padding: '15px 15px 0px 15px' }}>
+        Tovias Enigma
+        <Input
+          type='text'
+          label='Name'
+          id='sender'
+          value={sender}
+          icon={<Avatar icon={<span>{sender[0]}</span>} />}
+          onChange={handleChange}
+          required
+        />
+        <Input
+          type='text'
+          label='Message'
+          id='message'
+          value={message}
+          maxLength={120}
+          onChange={handleChange}
+          multiline
+          required
+        />
+        <DatePicker
+          label='Expiration date'
+          minDate={minimumDate}
+          id='date'
+          autoOk={true}
+          value={date}
+          onChange={handleChange}
+          sundayFirstDayOfWeek
+          required
+        />
+        <CardActions>
+          <Button label='ENCRYPT' id='encrypt' onClick={handleToggle} />
+          <Button label='DECRYPT' id='decrypt' onClick={handleToggle} />
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
+
+EnigmaCard.propTypes = {
+  sender: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  date: PropTypes.object.isRequired,
+  handleToggle: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
 
 export default EnigmaCard;
